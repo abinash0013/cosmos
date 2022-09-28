9d6dbbedf72cab31ba14fac3db5b71f18586bbb1
Abinash <abinash261997@gmail.com>
2022-09-28 19:11:32 +0530

    responsive work done 28sep

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -147,25 +147,41 @@ const Nav = () => {
 
     return (
         <Nav>
-            <div className="navbar">
+            <div className={menuIcon ? "navbar active" : "navbar"}>
                 <ul className="navbar-lists">
                     <li>
-                        <NavLink to="/" className={"navbar-link home-link"}>
+                        <NavLink
+                            to="/"
+                            className={"navbar-link home-link"}
+                            onClick={() => setMenuIcon(false)}
+                        >
                             Home
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/about" className={"navbar-link"}>
+                        <NavLink
+                            to="/about"
+                            className={"navbar-link"}
+                            onClick={() => setMenuIcon(false)}
+                        >
                             About
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/products" className={"navbar-link"}>
+                        <NavLink
+                            to="/products"
+                            className={"navbar-link"}
+                            onClick={() => setMenuIcon(false)}
+                        >
                             Products
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/contact" className={"navbar-link"}>
+                        <NavLink
+                            to="/contact"
+                            className={"navbar-link"}
+                            onClick={() => setMenuIcon(false)}
+                        >
                             Contact
                         </NavLink>
                     </li>
@@ -173,12 +189,26 @@ const Nav = () => {
                         <NavLink
                             to="/cart"
                             className="navbar-link cart-trolley--link"
+                            onClick={() => setMenuIcon(false)}
                         >
                             <FiShoppingCart className="cart-trolley" />
                             <span className="cart-total--item">10</span>
                         </NavLink>
                     </li>
                 </ul>
+
+                <div className="mobile-navbar-btn">
+                    <CgMenu
+                        name="menu-outline"
+                        className="mobile-nav-icon"
+                        onClick={() => setMenuIcon(true)}
+                    />
+                    <CgClose
+                        name="close-outline"
+                        className="mobile-nav-icon close-outline"
+                        onClick={() => setMenuIcon(false)}
+                    />
+                </div>
             </div>
         </Nav>
     );
