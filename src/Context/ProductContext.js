35d1382111bdd924528ec9e3c4d2512c9790c926
Abinash <abinash261997@gmail.com>
2022-10-06 23:37:24 +0530

    state management using usereducer hooks

--- src/Context/ProductContext.js
+++ src/Context/ProductContext.js
@@ -2,25 +2,40 @@
 // provider
 // consumer => useContext Hooks
 
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import axios from "axios";
+import { Reducer } from "./../reducer/productReducer.js";
 
 const AppContext = createContext();
 
 const API = "https://api.pujakaitem.com/api/products";
 
+const initialState = {
+    isLoading: false,
+    isError: false,
+    products: [],
+    featureProducts: [],
+};
+
 const AppProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
     const getProducts = async (url) => {
-        const res = await axios.get(url);
-        const products = await res.data;
-        console.log("products", products);
+        dispatch({ type: "SET_LOADING" });
+        try {
+            const res = await axios.get(url);
+            const products = await res.data;
+            dispatch({ type: "SET_API_DATA", payload: products });
+        } catch (error) {
+            dispatch({ type: "API_ERROR" });
+        }
     };
     useEffect(() => {
         getProducts(API);
     }, []);
 
     return (
-        <AppContext.Provider value={{ myName: "Abinash" }}>
+        <AppContext.Provider value={{ ...state }}>
             {children}
         </AppContext.Provider>
     );
---
+++ src/reducer/productReducer.js
@@ -0,0 +1,43 @@
+const productReducer = (state, action) => {
+    // if (action.type === "SET_LOADING") {
+    //     return {
+    //         ...state,
+    //         isLoading: true,
+    //     };
+    // }
+    // if (action.type === "API_ERROR") {
+    //     return {
+    //         ...state,
+    //         isLoading: false,
+    //         isError: true,
+    //     };
+    // }
+
+    switch (action.type) {
+        case "SET_LOADING":
+            return {
+                ...state,
+                isLoading: true,
+            };
+        case "SET_API_DATA":
+            const featureData = action.payload.filter((curElem) => {
+                return curElem.featured === true;
+            });
+            return {
+                ...state,
+                isLoading: false,
+                products: action.payload,
+                featureProducts: featureData,
+            };
+        case "SET_API_ERROR":
+            return {
+                ...state,
+                isLoading: true,
+            };
+
+        default:
+            return state;
+    }
+};
+
+export default productReducer;
